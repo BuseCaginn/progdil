fb3caea98740a85e98b5c95a55468471ab23e806
Buse Çağın <buse.cagin@bil.omu.edu.tr>
2016-01-11 23:55:08 +0200

    Update Rasgele_Proje_Ismi.php

--- Rasgele_Proje_Ismi.php
+++ Rasgele_Proje_Ismi.php
@@ -1,66 +1,66 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="tr">
 <head>
-  <meta charset="utf-8">
+	<meta charset="utf-8">
 </head>
 <body>
-<table border="1" bordercolor="#ff0000" align="center">
 
 <?php
+
 $tetikle = $_POST['tetikle'];
 $dil = $_POST['dil'];
 
-if ($tetikle == "1") {
-	if ($dil == "tr") {
-$sifatdizi = $sifatlar = array("Kırmızı", "Uzun", "Büyük", "Dar", "Sarı", "Tatlı", "Yuvarlak", "Şişman","Renkli");
+if ($tetikle == "1"){
 
-$isimdizi = $isimler = array("Dolap", "Çay", "Kutu", "Penguen", "Araba", "Sopa", "Kedi", "Masa","Balina");
+	if ($dil == "tr"){
+		$sifatlar = array("Kırmızı", "Uzun", "Büyük", "Dar", "Sarı", "Tatlı", "Yuvarlak", "Şişman","Renkli");
+		$isimler = array("Dolap", "Çay", "Kutu", "Penguen", "Araba", "Sopa", "Kedi", "Masa","Balina");
 	}
-	if ($dil == "en") {
-$sifatdizi = $sifatlar = array("Red", "Long", "Big", "Tight", "Yellow", "Sweet", "Round", "Fat","Color");
 
-$isimdizi = $isimler = array("Closet", "Tea", "Box", "Penguin", "Car", "Bat", "Cat", "Table","Whale");
+	if ($dil == "en"){
+		$sifatlar = array("Red", "Long", "Big", "Tight", "Yellow", "Sweet", "Round", "Fat","Color");
+		$isimler  = array("Closet", "Tea", "Box", "Penguin", "Car", "Bat", "Cat", "Table","Whale");
 	}
 	
 $tp_sifat = count($sifatlar);
 $tp_isim  = count($isimler);
+$i = 0;
 
-echo "<tr>";
-		for ($i=0; $i < $tp_sifat; $i++)
+echo " ";
+					
+	while ( $i < $tp_sifat) {
+		$sifat = array_rand($sifatlar, 1);
+		$isim  = array_rand($isimler, 1);
+							
+		echo "".$sifatlar[$sifat]."_".$isimler[$isim];
+		echo " ";
+		unset($isimler[$isim]);
+		unset($sifatlar[$sifat]);
+		echo "<br>";
 
-				{
-				  if (empty($sifatlar) )
-					$sifatlar = $sifatdizi;
-							$sifat = array_rand($sifatlar, 1);
-								$sifatyaz = $sifatlar[$sifat];
-							unset ($sifatlar[$sifat]);
-					echo "<td>".$sifatyaz."</td>". PHP_EOL;
-				}
-echo "</tr><tr>";		
-		for ($i=0; $i < $tp_isim; $i++)
+		$i += 1;
+	}
+	
+	if(empty($sifatlar) && empty($isimler)){
+		echo "_";
+	}
+		
+	echo " ";
+}
 
-				{
-				  if (empty($isimler))
-					$isimler  = $isimdizi;
-						$isim  = array_rand($isimler, 1);
-							$isimyaz  = $isimler[$isim];
-						unset ($isimler[$isim]);
-					echo "<td>".$isimyaz."</td>". PHP_EOL;
-				}
-echo "</tr>";
-}else{
+else {
 	echo "<center>Dil Seçiniz ...</center><br>";
 }		
+
 ?>
 
-</table>
 <br><br>
-<form action="sifatlar.php" method="POST">
+<form action="index.php" method="POST">
 <input type="hidden" name="tetikle" value="1">
 <input type="hidden" name="dil" value="tr">
 <center><button>Türkçe</button></center>
 </form>
 
-<form action="sifatlar.php" method="POST">
+<form action="index.php" method="POST">
 <input type="hidden" name="tetikle" value="1">
 <input type="hidden" name="dil" value="en">
 <center><button>English</button></center>
